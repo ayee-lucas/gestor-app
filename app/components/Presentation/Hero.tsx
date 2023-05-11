ee619969c04fe88b88b587f2ef48b0d15e6145fc
jmorales-2018317 <108350919+jmorales-2018317@users.noreply.github.com>
2023-05-11 16:15:22 +0000

    Presentation Design

--- app/components/Presentation/Hero.tsx
+++ app/components/Presentation/Hero.tsx
@@ -3,7 +3,6 @@ import React from "react";
 const Hero = () =>{
     return(
         <div className="relative dark:bg-[#09091F]" id="home">
-            <div className="absolute bg-black inset-0 opacity-60                                                          "/>
             <div aria-hidden="true" className="absolute inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20">
                 <div className="blur-[106px] h-56 bg-gradient-to-br from-primary to-purple-400 dark:from-blue-700"></div>
                 <div className="blur-[106px] h-32 bg-gradient-to-r from-cyan-400 to-sky-300 dark:to-indigo-600"></div>
--- app/components/Presentation/Start.tsx
+++ app/components/Presentation/Start.tsx
@@ -1,19 +1,37 @@
 import React from "react";
 import Image from "next/image";
-import logo from "../../../public/RoomFlyTransparent.png";
+import logo from "../../../public/Logos/RoomFlyTransparent.png";
+import { AiOutlineInstagram, AiOutlineMail, AiOutlineFacebook, AiOutlinePhone } from "react-icons/ai";
 
-const Start = () =>{
-    return(
-        <div className="relative bg-image bg-cover h-screen" id="home">
-            <div className="flex justify-between h-[15%]">
-                <div>
-                    <p>a</p>
+const Start = () => {
+    return (
+        <div className="relative bg-start bg-cover h-screen" id="home">
+
+            <div className="absolute inset-0 gradient opacity-80 z-0"/>
+
+            <div className="flex justify-between text-white h-[25%]">
+                <div className="flex flex-row-reverse justify-self-end items-center z-10 text-[25px] pl-16 space-x-5 space-x-reverse">
+                    <AiOutlineInstagram />
+                    <AiOutlineFacebook />
+                    <AiOutlineMail />
+                    <AiOutlinePhone />
                 </div>
-                <Image className="h-[200px] w-[150px]" src={logo} alt="" />
-                <div>
-                    <p>a</p>
+                <Image className="h-[190px] w-[180px] z-10" src={logo} alt="" />
+                <div className="flex flex-row-reverse justify-self-end items-center z-10 text-[20px] pr-16 space-x-6 space-x-reverse">
+                    <button>Sign up</button>
+                    <button>Login</button>
                 </div>
             </div>
+
+            <div className="flex justify-between text-white h-[75%]">
+                <div className="z-10 w-full px-48 pt-60">
+                    <h1 className="text-[75px]">Exclusive Offers</h1>
+                    <p className="text-[22px] mt-3 w-[60%]">Privileges that will turn your travel into memories beyond your expectations…</p>
+                </div>
+            </div>
+
+            
+
         </div>
     );
 }
