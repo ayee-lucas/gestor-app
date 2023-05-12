6b908dc403525d57f0d0cdf3413b1b2e682906a6
alopez-2018459 <alopez-2018459@kinal.edu.gt>
2023-05-12 00:30:17 -0600

    added features

--- app/account/signup/page.tsx
+++ app/account/signup/page.tsx
@@ -1,6 +1,10 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { playfair } from "@/app/fonts";
+
+import logo from "@/public/Logos/RoomFlyTransparent.png";
+import Image from "next/image";
 
 export default async function SingUp() {
   const session = await getServerSession(authOptions);
@@ -11,18 +15,28 @@ export default async function SingUp() {
   return (
     <div className="min-h-screen w-full grid grid-cols-2">
       <div className="w-full h-full "></div>
-      <div className="w-full h-full bg-indigo-700 flex flex-col justify-center items-start text-white px-8">
-        <h1 className="font-black text-3xl py-3">RoomFly</h1>
-        <h1 className=" text-4xl font-light">
+      <div className="w-full h-full relative bg-signup z-10 bg-cover flex flex-col justify-center items-start text-white px-8">
+        <div className="absolute inset-0 opacity-50 z-[5] bg-indigo-900"/>
+        <div className="w-full flex items-center z-[10]">
+
+          <Image src={logo} width={100} alt="logo" />
+
+          <h1 className="font-black text-4xl px-4 ">RoomFly</h1>
+
+        </div>
+        <div className="z-[10]">
+        <h1 className={`text-4xl ${playfair.className} font-black`}>
           Seamless Services and Exquisite Lodging for Unforgettable Getaways
         </h1>
-        <p className="py-2 pl-1">
+        <p className="py-3 pl-1">
           At RoomFly, we go beyond traditional lodging platforms by curating a
           handpicked selection of exquisite accommodations that embody elegance,
           style, and sophistication. From boutique hotels to luxurious resorts,
           our platform offers a diverse range of options to cater to your unique
           preferences
         </p>
+        </div>
+
       </div>
     </div>
   );
--- app/fonts.ts
+++ app/fonts.ts
@@ -1,6 +1,12 @@
-import { Montserrat } from "next/font/google";
+import { Montserrat, Playfair_Display } from "next/font/google";
 
 export const monsterrat = Montserrat({
     subsets: ['latin'],
     weight: ['100', '200', '400'],
+});
+
+export const playfair = Playfair_Display({
+    subsets: ['latin'],
+    weight: ['400', '500',  '600',],
+    style: ['normal'],
 });
