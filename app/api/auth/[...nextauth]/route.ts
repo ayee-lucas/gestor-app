7cec554925efb755364e3823f69c296f5a79e164
alopez-2018459 <alopez-2018459@kinal.edu.gt>
2023-05-11 23:40:42 -0600

    changed nextauth config

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth/next";
+import { NextAuthOptions } from "next-auth";
 import dbConnect from "@/app/utils/DatabaseConnection";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { NextResponse } from "next/server";
 
 dbConnect();
 
@@ -8,26 +10,17 @@ const url = process.env.NEXTAUTH_URL as string;
 
 console.log({ nextauth: `${url}api/User/login}` });
 
-const handler = NextAuth({
+export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
       name: "Credentials",
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
+     
       credentials: {
         username: { label: "Username", type: "text", placeholder: "jsmith" },
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
-        // You need to provide your own logic here that takes the credentials
-        // submitted and returns either a object representing a user or value
-        // that is false/null if the credentials are invalid.
-        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-        // You can also use the `req` object to obtain additional parameters
-        // (i.e., the request IP address)
+
         const res = await fetch("http://localhost:3000/api/User/login", {
           method: "POST",
           body: JSON.stringify({
@@ -39,17 +32,20 @@ const handler = NextAuth({
 
         const user = await res.json();
 
-        // If no error and we have user data, return it
-        if (res.status === 200 && user) {
+        console.log({user: user});
+
+        if (res.ok && user) {
           return user;
+        } else {
+          console.log(res);
+          return false;
         }
-        // Return null if user data could not be retrieved
-        return null;
       },
     }),
   ],
   pages: {
     signIn: "/account/login",
+    error: "/account/login",
   },
 
   callbacks: {
@@ -57,11 +53,13 @@ const handler = NextAuth({
       return { ...token, ...user };
     },
 
-    async session({session, token}){
+    async session({ session, token }) {
       session.user = token as any;
       return session;
-    }
+    },
   },
-});
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
