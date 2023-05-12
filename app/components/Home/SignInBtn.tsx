dbc2323f2b9119fa520e9bc7590e3756d963d588
Alan Lopez <108323739+alopez-2018459@users.noreply.github.com>
2023-05-12 16:57:40 +0000

    Register done

--- app/components/Home/SignInBtn.tsx
+++ app/components/Home/SignInBtn.tsx
@@ -1,5 +1,6 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import React from "react";
+import Link from "next/link";
 
 const SignInBtn = () => {
   const { data: session } = useSession();
@@ -14,19 +15,18 @@ const SignInBtn = () => {
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           onClick={() => signOut()}
         >
-          Sign out 
+          Sign out
         </button>
       </div>
     );
   }
   return (
-    <button
-      type="button"
+    <Link
+      href={"/account/signup"}
       className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      onClick={() => signIn()}
     >
       Get started
-    </button>
+    </Link>
   );
 };
 
