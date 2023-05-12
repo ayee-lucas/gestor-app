56f0c7bf7db2bb01ae0fa494bb60517eeb2ad351
cmendez-2018307 <108351423+cmendez-2018307@users.noreply.github.com>
2023-05-12 17:49:57 +0000

    PostEvent

--- app/api/Event/route.ts
+++ app/api/Event/route.ts
@@ -1,18 +1,48 @@
 import dbConnect from "@/app/utils/DatabaseConnection";
 import Event from "@/app/models/event";
-import { JWT } from "@/app/utils/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 dbConnect();
 
 
-export async function POST(request: Request) {
+export async function GET() {
     try{
-        
+        const event = await Event.find();
+        if(!event || event.length === 0){
+            return new NextResponse("No services found", {
+                status: 404,
+            }
+            )
+        }
     }catch (err){
         console.log(err);
         return new NextResponse(JSON.stringify({ message: " Error"  }),{
             status: 500,
         });
+    }
+}
+
+export async function POST(request: NextRequest) {
+    try{
+        const json = await request.json();
+        console.log({ DataRequest: json});
+
+        const data = new Event(json);
+        console.log({ EventCreated: data});
+
+        const event = await data.save();
+
+        return new NextResponse(JSON.stringify(event),{
+            status: 200,
+            headers: { "Content-Type": "application/json"},
+        });
+
+    } catch (err){
+        console.log({ err});
+        const error = {
+            message: "Error Saving Event",
+            error: err,
+        };
+        return new NextResponse(JSON.stringify(error), { status: 500});
     }
 }
