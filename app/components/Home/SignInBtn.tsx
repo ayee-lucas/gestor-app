63fca40263d0363c0273084ad12fa095bd83aa80
Alan Lopez <108323739+alopez-2018459@users.noreply.github.com>
2023-05-12 17:55:19 +0000

    Home styles added

--- app/components/Home/SignInBtn.tsx
+++ app/components/Home/SignInBtn.tsx
@@ -9,7 +9,7 @@ const SignInBtn = () => {
   if (session && session.user) {
     return (
       <div className="flex items-center justify-between min-w-[200px]">
-        <p>{`${session.user.name} ${session.user.surname}`}</p>
+        <p className="px-5 dark:text-white">{`${session.user.name} ${session.user.surname}`}</p>
         <button
           type="button"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
