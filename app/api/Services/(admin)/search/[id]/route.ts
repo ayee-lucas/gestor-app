31dad1694a9e4d96f826d071222ed33c71a0fbd9
pvasquez-2018044 <pvasquez-2018044@kinal.edu.gt>
2023-05-15 23:35:23 -0600

    Services, Rooms and Event firts try CRUDs

--- app/api/Services/(admin)/search/[id]/route.ts
+++ app/api/Services/(admin)/search/[id]/route.ts
@@ -32,3 +32,47 @@ export async function GET(request: Request, params: params) {
     });
   }
 }
+
+export async function PUT(request: Request, params: params) {
+  const id = params.params.id;
+  const data = await request.json();
+
+  try {
+    const service = await Service.findByIdAndUpdate(id, data, { new: true });
+    if (!service) {
+      return new NextResponse("Service not found", {
+        status: 404,
+      });
+    }
+    return new NextResponse(JSON.stringify(service), {
+      status: 200,
+    });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(JSON.stringify(err), {
+      status: 500,
+    });
+  }
+}
+
+export async function DELETE(request: Request, params: params) {
+  const id = params.params.id;
+
+  try {
+    const service = await Service.findByIdAndDelete(id);
+    if (!service) {
+      return new NextResponse("Service not found", {
+        status: 404,
+      });
+    }
+    return new NextResponse(JSON.stringify(service), {
+      status: 200,
+    });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(JSON.stringify(err), {
+      status: 500,
+    });
+  }
+}
+
