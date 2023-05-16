3951d8b980f9e399da118e885a3d3e13bcb8be68
alopez-2018459 <alopez-2018459@kinal.edu.gt>
2023-05-16 16:23:13 -0600

    Home page improvements

--- app/(user)/Home/page.tsx
+++ app/(user)/Home/page.tsx
@@ -1,3 +1,4 @@
+import HotelCardSm from "@/app/components/Home/HotelCardSm";
 import RoomCard from "@/app/components/Home/RoomCard";
 import { IRoom } from "@/app/models/rooms";
 
@@ -5,14 +6,12 @@ const url = process.env.NEXTAUTH_URL as string;
 
 async function getRooms() {
   const res = await fetch(`${url}/api/Rooms/`, {
-   cache: 'no-store'
+    cache: "no-store",
   });
 
   if (!res.ok) throw new Error(res.statusText);
 
   const rooms = await res.json();
-
-
 
   return rooms;
 }
@@ -23,17 +22,34 @@ export default async function Home() {
   console.log({ roomsData: roomsData });
 
   return (
-    <div className="w-full h-full p-4">
-      <h1 className="text-5xl font-bold">Rooms</h1>
-      <div className="w-full h-full py-4 flex flex-wrap justify-start gap-3">
-        {roomsData.map((room: IRoom) => (
-          <RoomCard
-            description={room.description}
-            price={room.price}
-            type={room.type}
-          />
-        ))}
+    <>
+      <div className="w-full h-full p-4">
+        <div className="w-full h-full bg-indigo-100 p-2 ">
+          <h1 className="text-2xl font-semibold">Sophisticated</h1>
+          <div className="grid grid-flow-col gap-4 col-auto overflow-x-auto p-6 scrollbar-thin scrollbar-thumb-blue-700 scrollbar-track-blue-300 scroll-smooth">
+            <HotelCardSm />
+            <HotelCardSm />
+            <HotelCardSm />
+            <HotelCardSm />
+            <HotelCardSm />
+          </div>
+        </div>
+
+        <h1 className="text-5xl font-bold py-10">Rooms</h1>
+        <div className="w-full h-full py-4 flex flex-wrap justify-around gap-y-5">
+          {roomsData.map((room: IRoom) => (
+            <RoomCard
+              hotel={room.hotel}
+              location={room.location}
+              smallDescription={room.shortescription}
+              image="https://images.unsplash.com/photo-1522771739844-6a9f6d5f14af?ixlib=rb-4.0.3&ixid=Mn wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
+              price={room.price}
+              type={room.type}
+              rating={room.rating}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- app/models/rooms.ts
+++ app/models/rooms.ts
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 import mongoose, { Schema, model, models, Document } from "mongoose";
 
@@ -8,6 +8,7 @@ export interface IRoom {
   description: string;
   price: number;
   rating: number;
+  shortescription: string;
   location: string;
   hotel: string;
   available: boolean;
@@ -26,9 +27,16 @@ const roomSchema = new Schema(
       type: String,
       required: [true, "Room type is required."],
     },
+    shortDescription: {
+      type: String,
+      required: [true, "Room short description is required."],
+      maxLength: 50,
+    },
+
     description: {
       type: String,
       required: [true, "Room description is required."],
+      maxLength: 200,
     },
     price: {
       type: Number,
@@ -37,12 +45,14 @@ const roomSchema = new Schema(
     rating: {
       type: Number,
       required: [true, "Room rating is required."],
+      min: 1,
+      max: 5,
     },
-    location:{
+    location: {
       type: String,
       required: [true, "Room location is required."],
     },
-    hotel:{
+    hotel: {
       type: String,
       required: [true, "Room hotel is required."],
     },
