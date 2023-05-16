0aca79e4c3b9304431891fb9ca7d4987eb141af5
alopez-2018459 <alopez-2018459@kinal.edu.gt>
2023-05-16 11:23:20 -0600

    Data fetched

--- app/(user)/Home/page.tsx
+++ app/(user)/Home/page.tsx
@@ -1,7 +1,40 @@
-export default function Home() {
+import RoomCard from "@/app/components/Home/RoomCard";
+import { IRoom } from "@/app/models/rooms";
+
+const url = process.env.NEXTAUTH_URL as string;
+
+async function getRooms() {
+  const res = await fetch(`${url}/api/Rooms/`, {
+   cache: 'no-store'
+  });
+
+  if (!res.ok) throw new Error(res.statusText);
+
+  const rooms = await res.json();
+
+
+
+  return rooms;
+}
+
+export default async function Home() {
+  const roomsData = await getRooms();
+
+  console.log({ roomsData: roomsData });
+
   return (
     <div className="w-full h-full p-4">
-      <h1>Home Page</h1>
+      <h1 className="text-5xl font-bold">Rooms</h1>
+      <div className="w-full h-full py-4 flex flex-wrap justify-start gap-3">
+        {roomsData.map((room: IRoom) => (
+          <RoomCard
+          location={room.location}
+            description={room.description}
+            price={room.price}
+            type={room.type}
+          />
+        ))}
+      </div>
     </div>
   );
 }
