1639f13cd3ea2e9f5d55551a618005a1fe1cb66b
falegria-2018246 <falegria-2018246@kinal.edu.gt>
2023-05-17 09:54:32 -0600

    Model Bill

---
+++ app/models/bill.ts
@@ -0,0 +1,89 @@
+"use strict";
+
+import mongoose, { Schema, model, models, Document } from "mongoose";
+
+export interface IBill {
+  issueDate: Date;
+  nit: string;
+  itemsSubtotal: Number;
+  servicesSubtotal: Number;
+  total: number;
+
+}
+
+export interface IBillModel extends IBill, Document {}
+
+const billSchema = new Schema(
+  {
+    issueDate: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "User "],
+    },
+    nit: {
+      type: String,
+      default: "C/F",
+    },
+    roomPrice: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Room",
+    },
+    eventType: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+    },
+    items: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        quantity: {
+          type: Number,
+        },
+        unitPrice: {
+          type: Number,
+        },
+      },
+    ],
+    services: [
+      {
+        service: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Service",
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    itemsSubtotal: {
+      type: Number,
+      default: 0,
+    },
+    servicesSubtotal: {
+      type: Number,
+      default: 0,
+    },
+    total: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+const Bill = (models.Bill ||
+  mongoose.model<IBillModel>("Bill", billSchema)) as typeof models.Bill &
+  (new (args: any) => IBillModel);
+
+export default Bill;
