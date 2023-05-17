f1f091430c1272956eeea0940bcdf89bfb2a4ce9
alopez-2018459 <alopez-2018459@kinal.edu.gt>
2023-05-16 18:05:24 -0600

    Fixed animation

--- app/components/elements/UserBlock.tsx
+++ app/components/elements/UserBlock.tsx
@@ -12,20 +12,19 @@ const UserBlock = ({ setShow, show }: Props) => {
 
   const user = session?.user;
 
-
   return (
     <div
       className={
         show
-          ? "absolute right-5 top-2  w-[50px] h-[50px] transition-all "
-          : "absolute right-5 top-2 px-2 py-2  w-[300px] h-[60px] transition-all bg-slate-50 bg-opacity-80 rounded-xl flex items-center gap-16"
+          ? "absolute overflow-hidden right-5 top-2  w-[50px] h-[50px] transition-all select-none cursor-pointer "
+          : "absolute overflow-hidden right-5 top-2 px-2 py-2  w-[300px] h-[60px] transition-all bg-slate-50 bg-opacity-80 rounded-xl flex items-center gap-16 select-none cursor-pointer"
       }
+      onClick={() => setShow(!show)}
     >
       <button
         className={
           "px-1 py-1 bg-indigo-100 flex max-w-[50px] items-center justify-center rounded-full transition-all"
         }
-        onClick={() => setShow(!show)}
       >
         <Image
           src={
@@ -37,8 +36,7 @@ const UserBlock = ({ setShow, show }: Props) => {
           className="rounded-full"
         />
       </button>
-      {show ? null :`${user?.name} ${user?.surname}`}
-
+      {show ? null : `${user?.name} ${user?.surname}`}
     </div>
   );
 };
